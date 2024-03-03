879a49cd79b3358f2f8a602345f5c9fdc42015bb
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-03-04 00:10:58 +0200

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Vlad Furtuna on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
